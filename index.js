953639835a7e665192526b95d919ea55edd94bb3
chrisjacobs101 <chrisjacobs101@gmail.com>
2017-12-15 20:34:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === true){
-    return "I can't hear you!";
-  } else if (string.toUpperCase() === true) {
+  if (string.toLowerCase() === string){
+    return "I can\'t hear you!";
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
