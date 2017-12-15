67c7fcf46ab45a0fb11ea5a521005e0566d7b0d7
chrisjacobs101 <chrisjacobs101@gmail.com>
2017-12-15 20:30:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ function shout(string) {
   return string.toUpperCase();
 }
 
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === true){
+    return "I can't hear you!";
+  } else if (string.toUpperCase() === true) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+
